39255fde2cf294667e6eccfbdb1a294f44cfa3ec
patrickhallen1992 <patrickh.allen1992@gmail.com>
2024-06-06 09:26:45 -0400

    grade_calculator.py

--- grade_calculator.py
+++ grade_calculator.py
@@ -1,15 +1,17 @@
 def calculate_average_grade():
     # Prompt the user for their name and store it in the student_name variable
-    student_name = ""
+    student_name = input("Patrick Allen")
 
     # Prompt the user for their scores in Math, Science, and English
     # Store the scores in the respective variables: math_score, science_score, english_score
-    math_score = 0
-    science_score = 0
-    english_score = 0
-
+    math_score = input("90")
+    science_score = input("80")
+    english_score = input("70")
+    math_score = int(math_score)
+    science_score = int(science_score)
+    english_score = int(english_score)
     # Calculate the average grade
-    average_grade = 0
+    average_grade = (math_score + science_score +english_score) / 3
 
     # Return the student's name and their average grade
     return student_name, average_grade
@@ -19,4 +21,5 @@ if __name__ == '__main__':
     student_name, average_grade = calculate_average_grade()
 
     # Print the student's name and their average grade
-    print(f"")
+    print(f"Student Name: {student_name}")
+    print(f"Average Grade: {average_grade}")
